b0c5d8428133b16e1f9b318ed641406f3f2ba2d2
kejace <kejace@gmail.com>
2020-10-13 16:31:39 -0700

    ENV vars works with envify

--- app/UI/Config.js
+++ app/UI/Config.js
@@ -1,9 +1,4 @@
 "use strict";
 
-exports.providerURLImpl = function() {
-  return process.env.PROVIDER_URL; // eslint-disable-line no-undef
-};
-
-exports.relayableNFTImpl = function() {
-  return process.env.RELAYABLE_NFT; // eslint-disable-line no-undef
-};
+exports.providerURLImpl = process.env.PROVIDER_URL; // eslint-disable-line no-undef
+exports.relayableNFTImpl = process.env.RELAYABLE_NFT; // eslint-disable-line no-undef
