b2870a3a6a163aafd40012f2fcbde895cc65ed90
blinky3713 <martin@foam.space>
2020-10-26 14:19:45 -0700

    map and map container

--- app.dhall
+++ app.dhall
@@ -1,6 +1,6 @@
 { name = "foam5g-app"
 , dependencies =
-  [ "console", "effect", "halogen", "ocelot", "psci-support", "web3", "react", "web-mercator", "deck-gl", "react-map-gl" ]
+  [ "aff-bus", "console", "effect", "halogen", "ocelot", "psci-support", "web3", "react", "web-mercator", "deck-gl", "react-map-gl" ]
 , packages = ./packages.dhall
 , sources = [ "dapp/Contracts/**/*.purs", "app/**/*.purs" ]
 }
--- app/UI/Component/Map/Component.purs
+++ app/UI/Component/Map/Component.purs
@@ -2,13 +2,118 @@ module UI.Component.Map.Component where
 
 import Prelude
 
-import Data.Newtype (unwrap)
+import Control.Lazy (fix)
+import Data.Newtype (un, unwrap)
+import Data.Tuple (snd)
 import DeckGL as DeckGL
+import DeckGL.BaseProps as BaseLayer
 import DeckGL.Layer.Icon as Icon
+import Effect (Effect)
+import Effect.Aff (error, launchAff_)
+import Effect.Aff.AVar (AVar)
+import Effect.Aff.AVar as AVar
+import Effect.Aff.Bus as Bus
+import Effect.Class (liftEffect)
+import Effect.Uncurried (mkEffectFn1)
+import Foreign.Object as O
+import MapGL (Viewport, ClickInfo)
 import MapGL as MapGL
+import Math (pow)
 import React as R
+import Record (disjointUnion)
 import WebMercator.LngLat (LngLat)
 import WebMercator.LngLat as LngLat
+
+--------------------------------------------------------------------------------
+
+
+data MapMessages
+  = OnViewportChange Viewport
+  | OnClick ClickInfo
+
+data Commands
+  = SetViewport' Viewport
+  | AskViewport' (AVar Viewport)
+
+data Messages
+  = IsInitialized (Bus.BusW Commands)
+  | PublicMsg MapMessages
+
+--------------------------------------------------------------------------------
+
+type Props =
+  { messages :: Bus.BusW Messages
+  , width :: Number
+  , height :: Number
+  }
+
+type State =
+  { command :: Bus.BusRW Commands
+  , viewport :: MapGL.Viewport
+  }
+
+mapClass :: R.ReactClass Props
+mapClass = R.component "Map" \this -> do
+  command <- Bus.make
+  { messages, width, height } <- R.getProps this
+  launchAff_ $ Bus.write (IsInitialized $ snd $ Bus.split command) messages
+  pure
+    { componentDidMount: componentDidMount this
+    , componentWillUnmount: componentWillUnmount this
+    , render: render this
+    , state:
+        { viewport: MapGL.Viewport
+          { width
+          , height
+          , longitude: -74.00539284665783
+          , latitude: 40.70544878575082
+          , zoom: 10.822714855509464
+          , pitch: 0.0
+          , bearing: 0.0
+          }
+        , command
+        }
+    }
+  where
+    componentWillUnmount :: R.ReactThis Props State -> R.ComponentWillUnmount
+    componentWillUnmount this = R.getState this >>= \{ command } ->
+      launchAff_ $ do
+        props <- liftEffect $ R.getProps this
+        Bus.kill (error "kill from componentWillUnmount") command
+
+    componentDidMount :: R.ReactThis Props State -> R.ComponentDidMount
+    componentDidMount this = do
+      { command } <- R.getState this
+      launchAff_ $ fix \loop -> do
+        msg <- Bus.read command
+        case msg of
+          SetViewport' vp -> liftEffect $ R.modifyState this _{viewport = vp}
+          AskViewport' var -> liftEffect (R.getState this) >>= \{viewport} -> AVar.put viewport var
+        loop
+
+    render :: R.ReactThis Props State -> R.Render
+    render this = do
+      { messages } <- R.getProps this
+      { viewport } <- R.getState this
+      pure $ R.createElement MapGL.mapGL
+        (un MapGL.Viewport viewport `disjointUnion`
+        { onViewportChange: mkEffectFn1 $ \newVp -> do
+            launchAff_ $ Bus.write (PublicMsg $ OnViewportChange newVp) messages
+            void $ R.modifyState this _{viewport = newVp}
+        , onClick: mkEffectFn1 $ \info -> do
+            launchAff_ $ Bus.write (PublicMsg $ OnClick info) messages
+        , mapStyle: mapStyle
+        , mapboxApiAccessToken: mapboxApiAccessToken
+        , onLoad: pure unit
+        , dragRotate: true
+        , touchZoom: true
+        , touchRotate: true
+        })
+        [ R.createLeafElement iconLayerClass 
+            { viewport
+            , data: []
+            } 
+        ]
 
 
 --------------------------------------------------------------------------------
@@ -21,17 +126,14 @@ type MapPoint =
   }
 
 -- | Icon Layer Component
-type Props =
+type IconProps =
   { viewport :: MapGL.Viewport
   , data :: Array MapPoint
-  , iconMapping :: Icon.IconMapping
-  , iconAtlas :: String
-  , discreteZoom :: Int
   }
 
-type State = Record ()
+type MapState = Record ()
 
-iconLayerClass :: R.ReactClass Props
+iconLayerClass :: R.ReactClass IconProps
 iconLayerClass = R.component "IconLayer" \this -> do
   props <- R.getProps this
   pure
@@ -47,15 +149,25 @@ iconLayerClass = R.component "IconLayer" \this -> do
           iconLayer = Icon.makeIconLayer $
                         ( Icon.defaultIconProps { id = "icon"
                                                 , data = map (\m -> { point: m }) props.data
-                                                , pickable = false
+                                                , pickable = true
                                                 , visible = true
-                                                , iconAtlas = props.iconAtlas
-                                                , iconMapping = props.iconMapping
+                                                , iconAtlas = "/beacon.png"
+                                                , iconMapping = 
+                                                    O.insert "beacon"
+                                                      { x: 0
+                                                      , y: 0
+                                                      , width:  36
+                                                      , height: 36
+                                                      , mask: true
+                                                      }
+                                                      O.empty
                                                 , opacity = 1.0
-                                                , sizeScale = 2.0 * iconSize
                                                 , getPosition = \{point} -> pointLngLat point
-                                                , getIcon = const "icon"
-                                                , getSize = const 1.0
+                                                , getIcon = const "beacon"
+                                                , getSize = const $ 
+                                                    let sizeScale = (min 1.0 $ pow 1.5 (vp.zoom - 14.0)) * 18.0 * 2.0 -- copy/pasted from foam.visualizer
+                                                    in sizeScale * 1.0
+                                                , onClick = mkEffectFn1 onClickPoint
                                                 })
       pure
         $ R.createLeafElement DeckGL.deckGL
@@ -73,3 +185,8 @@ mapStyle = "mapbox://styles/mapbox/dark-v9"
 
 mapboxApiAccessToken :: String
 mapboxApiAccessToken = "pk.eyJ1IjoiYmxpbmt5MzcxMyIsImEiOiJjamVvcXZtbGYwMXgzMzNwN2JlNGhuMHduIn0.ue2IR6wHG8b9eUoSfPhTuQ"
+
+onClickPoint
+  :: BaseLayer.PickingInfo {point :: MapPoint}
+  -> Effect  Boolean
+onClickPoint = const $ pure true
