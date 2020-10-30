e8bbc6ad5059cc513fd499177ccb1230713b7a85
Kristoffer Josefsson <kejace@gmail.com>
2020-10-30 12:08:49 -0400

    Kristoffer/ping (#15)
    * copy modules from foam.vis
    
    * Ping Layer
    
    * added ping purs files
    
    * removed all files
    
    * Webpack seems to work with deck.gl. Needs testing
    
    * Devserver on 0.0.0.0
    
    * Logging from entrypoint
    
    * print log in entrypoint before app starts
    
    * Cleanup
    
    * compiles without warnings
    
    * compiles without warnings but no animations
    
    * keep an increasing clock
    
    * Works. Could use some polish in terms of design
    
    Co-authored-by: blinky3713 <martin@foam.space>

--- app/UI/Config.purs
+++ app/UI/Config.purs
@@ -6,7 +6,7 @@ import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
 import Network.Ethereum.Core.Signatures (Address, mkAddress)
-import Network.Ethereum.Web3 (Provider, metamaskProvider, mkHexString)
+import Network.Ethereum.Web3 (Provider, httpProvider, mkHexString)
 import Partial.Unsafe (unsafeCrashWith)
 
 foreign import providerURLImpl :: String
@@ -28,7 +28,7 @@ makeAppEnv = do
   case mkHexString relayableNFTImpl >>= mkAddress of
     Nothing -> unsafeCrashWith $ "Unable to parse RelayableNFT Address:" <> relayableNFTImpl
     Just relayableNFT -> do 
-      web3Provider <- metamaskProvider
+      web3Provider <- httpProvider "http://localhost:8545"
       pure $ AppEnv 
         { web3Provider
         , contracts: {relayableNFT}
