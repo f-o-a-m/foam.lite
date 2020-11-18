661e16f576246901beb05e3e2a7701fb589086c3
Ilya Ostrovskiy <iostat@users.noreply.github.com>
2020-11-17 19:29:16 -0500

    more informative logging when FungibleToken is used from an existing contract

--- dapp/DApp/Deploy/Script.purs
+++ dapp/DApp/Deploy/Script.purs
@@ -7,7 +7,7 @@ import Chanterelle.Internal.Logging (LogLevel(..), log)
 import Chanterelle.Internal.Types (DeployConfig(..), DeployM, ContractConfig)
 import Control.Monad.Reader (ask)
 import DApp.Deploy.ContractConfig (Contract, DeployResults, fungibleTokenConfig, relayableNFTConfig)
-import Data.Either (Either(..), fromRight)
+import Data.Either (Either(..))
 import Data.Lens ((?~))
 import Data.Maybe (Maybe(..), fromJust)
 import Data.Validation.Semigroup as V
@@ -19,8 +19,16 @@ import Partial.Unsafe (unsafeCrashWith, unsafePartial)
 vToEither :: forall err res. V.V err res -> Either err res
 vToEither = V.unV Left Right
 
-wrapDeployed :: forall a. String -> String -> ContractConfig a -> DeployM { deployAddress :: Address, deployHash :: HexString, deployArgs :: Record a }
-wrapDeployed h a args = pure { deployHash: mkHexString' h, deployAddress: mkAddress' a, deployArgs: unsafePartial fromRight (vToEither args.unvalidatedArgs) }
+wrapDeployed :: forall a
+              . { description :: String
+                , deployHash :: String
+                , deployAddress :: String
+                , deployArgs :: Record a
+                }
+              -> DeployM { deployAddress :: Address, deployHash :: HexString, deployArgs :: Record a }
+wrapDeployed rec = do
+  log Info $ "Using " <> rec.description <> " at " <> show rec.deployAddress
+  pure { deployHash: mkHexString' rec.deployHash, deployAddress: mkAddress' rec.deployAddress, deployArgs: rec.deployArgs }
 
 mkAddress' :: String -> Address
 mkAddress' addr = case mkAddress =<< mkHexString addr of
@@ -44,22 +52,24 @@ deployScript = do
   let bigGasLimit = unsafePartial fromJust $ parseBigNumber decimal "5000000"
       txOpts = defaultTransactionOptions # _from ?~ primaryAccount
                                          # _gas ?~ bigGasLimit
-                                         # _gasPrice ?~ (unsafePartial fromJust $ parseBigNumber decimal "2000000000")
+                                         # _gasPrice ?~ (unsafePartial fromJust $ parseBigNumber decimal "60000000000")
 
       deployContract' :: forall args. ContractConfig args -> DeployM (Contract args)
       deployContract' = deployContract txOpts
       initialSupply = mkUIntN' s256 "1000000000000000000000000000"
   fungibleToken <- 
     if networkID == 1
-    then pure { deployHash: mkHexString' "0x6cadd5e8960f526803f23a72a9232492a028502a81aa71fb21beb6907d345313"
-              , deployAddress: mkAddress' "0x4946fcea7c692606e8908002e55a582af44ac121"
-              , deployArgs: { initialSupply }
-              }
+    then wrapDeployed { description: "Mainnet FOAM Token for FungibleToken"
+                      , deployHash: "0x6cadd5e8960f526803f23a72a9232492a028502a81aa71fb21beb6907d345313"
+                      , deployAddress: "0x4946fcea7c692606e8908002e55a582af44ac121"
+                      , deployArgs: { initialSupply }
+                      }
     else if networkID == 4
-    then pure { deployHash: mkHexString' "0x37a7f0318f8b6689a4667768a49050d4878b38323838ef8f88f1dc8173295597"
-              , deployAddress: mkAddress' "0xf450fab8717f7910fc508d2224cad0bf3321517a"
-              , deployArgs: { initialSupply }
-              }
+    then wrapDeployed { description: "Rinkeby FOAM Token for FungibleToken"
+                      , deployHash: "0x37a7f0318f8b6689a4667768a49050d4878b38323838ef8f88f1dc8173295597"
+                      , deployAddress: "0xf450fab8717f7910fc508d2224cad0bf3321517a"
+                      , deployArgs: { initialSupply }
+                      }
     else deployContract' $ fungibleTokenConfig { initialSupply }
   relayableNFT <- deployContract' $ relayableNFTConfig { fungibleToken: fungibleToken.deployAddress }
   pure $ { fungibleToken, relayableNFT }
